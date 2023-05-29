7ec76db92124026f3f44374eb632453c78cdf2e9
Ronald Record <github@ronrecord.com>
2023-05-29 11:58:25 -0700

    Update community colorscheme names

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,8 +3,8 @@ return {
   -- Available plugins can be found at https://github.com/AstroNvim/astrocommunity
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.colorscheme.nightfox", enabled = false },
-  { import = "astrocommunity.colorscheme.kanagawa", enabled = true },
+  { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = false },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
   { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { -- further customize the options set by the community
